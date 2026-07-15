b803dd21a1844fc07947be4caaeda65056a2e9b7
Zhongyue Lin <101193087+LeoLin990405@users.noreply.github.com>
2026-07-15 11:30:00 +0800

    fix(xlsx): correct ATAN2 argument order
    Excel ATAN2 takes arguments as (x_num, y_num), while .NET Math.Atan2 expects (y, x). Swap the forwarded coordinates so evaluated formulas match Excel's function contract.
    
    Refs #211.

--- src/officecli/Core/Formula/FormulaEvaluator.Functions.cs
+++ src/officecli/Core/Formula/FormulaEvaluator.Functions.cs
@@ -74,7 +74,7 @@ internal partial class FormulaEvaluator
             "PI" => FR(Math.PI),
             "SIN" => FR(Math.Sin(num(0))), "COS" => FR(Math.Cos(num(0))), "TAN" => FR(Math.Tan(num(0))),
             "ASIN" => FR(Math.Asin(num(0))), "ACOS" => FR(Math.Acos(num(0))), "ATAN" => FR(Math.Atan(num(0))),
-            "ATAN2" => FR(Math.Atan2(num(0), num(1))),
+            "ATAN2" => FR(Math.Atan2(num(1), num(0))),
             "SINH" => FR(Math.Sinh(num(0))), "COSH" => FR(Math.Cosh(num(0))), "TANH" => FR(Math.Tanh(num(0))),
             "ASINH" => FR(Math.Asinh(num(0))), "ACOSH" => FR(Math.Acosh(num(0))), "ATANH" => FR(Math.Atanh(num(0))),
             "DEGREES" => FR(num(0) * 180.0 / Math.PI),
